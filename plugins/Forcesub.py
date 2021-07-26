922a50aeb388ccb095ad5d559f47171fec007be9
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-07-26 11:58:03 +0530

    Create Forcesub.py

---
+++ plugins/Forcesub.py
@@ -0,0 +1,55 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
+OWNER_ID = os.environ.get('OWNER_ID')
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+
+@Client.on_message(filters.private & filters.incoming)
+async def forcesub(c, m):
+    owner = await c.get_users(int(OWNER_ID))
+    if UPDATE_CHANNEL:
+        try:
+            user = await c.get_chat_member(UPDATE_CHANNEL, m.from_user.id)
+            if user.status == "kicked":
+               await m.reply_text("**Hey you are banned 😜**", quote=True)
+               return
+        except UserNotParticipant:
+            buttons = [[InlineKeyboardButton(text='Updates Channel 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
+            if m.text:
+                if (len(m.text.split(' ')) > 1) & ('start' in m.text):
+                    chat_id, msg_id = m.text.split(' ')[1].split('_')
+                    buttons.append([InlineKeyboardButton('🔄 Refresh', callback_data=f'refresh+{chat_id}+{msg_id}')])
+            await m.reply_text(
+                f"**Hey {m.from_user.mention(style='md')} you need join My updates channel in order to use me 😉**\n\n"
+                "__Press the Following Button to join Now 👇__",
+                reply_markup=InlineKeyboardMarkup(buttons),
+                quote=True
+            )
+            return
+        except Exception as e:
+            print(e)
+            await m.reply_text(f"Something Wrong. Please try again later or contact {owner.mention(style='md')}", quote=True)
+            return
+    await m.continue_propagation()
+
+
+@Client.on_callback_query(filters.regex('^refresh'))
+async def refresh_cb(c, m):
+    owner = await c.get_users(int(OWNER_ID))
+    if UPDATE_CHANNEL:
+        try:
+            user = await c.get_chat_member(UPDATE_CHANNEL, m.from_user.id)
+            if user.status == "kicked":
+               try:
+                   await m.message.edit("**Hey you are banned 😜**")
+               except:
+                   pass
+               return
+        except UserNotParticipant:
+            await m.answer('You are not yet joined our channel. First join and then press refresh button 🤤', show_alert=True)
+            return
+        except Exception as e:
+            print(e)
+            await m.message.edit(f"Something Wrong. Please try again later or contact {owner.mention(style='md')}")
+            return
