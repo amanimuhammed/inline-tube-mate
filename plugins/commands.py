2a329edd266d2dc0e2ef56e41de8bfdb7a01d6dc
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-07-26 07:22:19 +0530

    May be 🙄

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,5 +11,5 @@ from library.buttons import reply_markup_start
 
 @Client.on_message(filters.private & filters.command(['start', 'help']))
 async def start_bot(bot, m: Message):
-    await m.reply_text(Presets.WELCOME_MSG.format(m.from_user.first_name),
+    await m.reply_text(Presets.WELCOME_MSG.format{m.from_user.mention(style='md')}**,
                        reply_markup=reply_markup_start)
